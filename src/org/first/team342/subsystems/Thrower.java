29206e5352f9f3f82da0ae66a5facc67c8914778
Adam Brightwell <adam.brightwell@gmail.com>
2012-01-14 14:03:36 -0500

    added Thrower subsystem template.

---
+++ src/org/first/team342/subsystems/Thrower.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.first.team342.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author abrightwell
+ */
+public class Thrower extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
