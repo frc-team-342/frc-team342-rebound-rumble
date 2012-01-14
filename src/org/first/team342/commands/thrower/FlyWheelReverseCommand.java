95362939a8440a6b4fce655cdc424976a2921464
Jafe Sellers <jafesellers@gmail.com>
2012-01-14 14:41:09 -0500

    added thrower forward and reverse comands

---
+++ src/org/first/team342/commands/thrower/FlyWheelReverseCommand.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.first.team342.commands.thrower;
+
+import org.first.team342.commands.CommandBase;
+
+/**
+ *
+ * @author Team 342
+ */
+public class FlyWheelReverseCommand extends CommandBase {
+    private Thrower thrower;
+    public FlyWheelReverseCommand() {
+        this.thrower = Thrower.getInstance();
+        requires(this.thrower);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        this.thrower.throwRverse();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
