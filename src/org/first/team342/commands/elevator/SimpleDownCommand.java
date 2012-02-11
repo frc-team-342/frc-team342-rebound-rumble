ac66e7623ad1604203854843dd0310b239814be2
Jafe Sellers <jafesellers@gmail.com>
2012-02-11 10:50:25 -0500

    added simple elevator commands

---
+++ src/org/first/team342/commands/elevator/SimpleDownCommand.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.first.team342.commands.elevator;
+
+import org.first.team342.commands.CommandBase;
+import org.first.team342.subsystems.Elevator;
+
+/**
+ *
+ * @author Team 342
+ */
+public class SimpleDownCommand extends CommandBase {
+    private Elevator elevator;
+    
+    public SimpleDownCommand() {
+        elevator = Elevator.getInstance();
+        requires(elevator);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        elevator.simpleDown();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
